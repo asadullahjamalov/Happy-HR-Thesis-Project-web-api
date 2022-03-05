f450ae6786720f37a9beca784e63c90064bd8ee4
asadullahjamalov <asadullahnj@code.edu.az>
2022-03-05 09:46:58 +0400

    creating project

---
+++ src/main/java/az/thesis/happyhrthesisprojectwebapi/HappyHrThesisProjectWebApiApplication.java
@@ -0,0 +1,13 @@
+package az.thesis.happyhrthesisprojectwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyHrThesisProjectWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyHrThesisProjectWebApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/thesis/happyhrthesisprojectwebapi/HappyHrThesisProjectWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package az.thesis.happyhrthesisprojectwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyHrThesisProjectWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
